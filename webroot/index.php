f55248812dded4054ab7ef383134d9d6ee53e2d3
YodaDNK <yoda.dnk.82@gmail.com>
2017-05-18 09:13:08 +0300

    Test mod-rewrite

---
+++ webroot/index.php
@@ -0,0 +1,12 @@
+<?php
+/*error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+register_shutdown_function(function(){
+   $err = error_get_last();
+   echo "<pre>".print_r($err)."</pre>";
+   die();
+});*/
+
+phpinfo();
